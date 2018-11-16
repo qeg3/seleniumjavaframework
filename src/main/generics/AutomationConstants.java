123c5d824f5d3857623e2b8a84d790a2749a281d
Amarnath Pullalarevu <apullalarevu-c@sbgtv.com>
2018-11-16 14:20:26 +0530

    Updated BaseTest methods

--- src/main/generics/AutomationConstants.java
+++ src/main/generics/AutomationConstants.java
@@ -6,9 +6,17 @@ public interface AutomationConstants {
 
     public static  final String CHROME_WIN_VALUE="src/Test/resources/Drivers/chromedriver.exe";
 
+    public static  final String CHROME_MAC_VALUE="/usr/local/bin/chromedriver";
+
     public static  final String GECKO_KEY="webdriver.gecko.driver";
 
     public static  final String GECKO_VALUE="src/Test/resources/Drivers/geckodriver.exe";
+
+    public static  final String GECKO_MAC_VALUE="";
+
+    public static  final String SAFARI_KEY="";
+
+    public static  final String SAFARI_VALUE="";
 
     public static final String REPORTS_PATH="/Reports/TennisChannel_AutomationReport.html";
 }
--- src/main/generics/BaseTest.java
+++ src/main/generics/BaseTest.java
@@ -4,9 +4,16 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
-public class BaseTest implements AutomationConstants{
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest implements AutomationConstants{
     public static WebDriver driver;
     public static ExtentHtmlReporter report;
     public static ExtentTest reporter;
@@ -16,8 +23,45 @@ public class BaseTest implements AutomationConstants{
     @BeforeSuite
     public WebDriver open_Browser(String platform, String browser){
 
+        if(platform.equalsIgnoreCase("Windows")){
+
+            if(browser.equalsIgnoreCase("Chrome")){
+                System.setProperty(CHROME_KEY,CHROME_WIN_VALUE);
+                driver = new ChromeDriver();
+            }
+            else if(browser.equalsIgnoreCase("FireFox")) {
+                System.setProperty(GECKO_KEY, GECKO_VALUE);
+                driver = new FirefoxDriver();
+            }
+        }
+        else if (platform.equalsIgnoreCase("Mac")){
+
+            if(browser.equalsIgnoreCase("Chrome")){
+                System.setProperty(CHROME_KEY,CHROME_MAC_VALUE);
+                driver = new ChromeDriver();
+            }
+            else if (browser.equalsIgnoreCase("Safari")){
+                System.setProperty(SAFARI_KEY,SAFARI_VALUE);
+                driver = new SafariDriver();
+            }
+            else if(browser.equalsIgnoreCase("FireFox")) {
+                System.setProperty(GECKO_KEY, GECKO_MAC_VALUE);
+                driver = new FirefoxDriver();
+            }
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+
         return driver;
     }
+
+    @BeforeClass
+    public void initalizeGlobalvariables(){
+
+    }
+
     @BeforeTest
     public void configReports(){
         report = new ExtentHtmlReporter(System.getProperty("user.dir")+REPORTS_PATH);
@@ -34,17 +78,32 @@ public class BaseTest implements AutomationConstants{
         extent.attachReporter(report);
 
     }
+
     @BeforeMethod
-    public void startTest(){
+    public void startTest(Method method){
+        String testName=method.getName();
+        reporter=extent.createTest(testName);
+        reporter.info("Started Executing: "+testName);
 
     }
-    @AfterMethod
-    public void endTest(){
 
+    @AfterMethod
+    public void endTest(Method method){
+        String testName=method.getName();
+        if(testName.equals(ITestResult.SUCCESS)){
+            reporter.pass(testName+" : has Successfully Executed");
+        }else if (testName.equals(ITestResult.FAILURE)){
+            reporter.fail(testName+ ": has Failed to Execute");
+        }else if(testName.equals(ITestResult.SKIP)){
+            reporter.skip(testName+" : has got Skipped from Exection");
+        }
     }
 
     @AfterSuite
     public void tearDown() {
+        driver.close();
+        driver.quit();
+        report.flush();
 
     }
 }
