6b0e1b39cd932c9eab985072e39b2ce58e60b3b0
Amarnath Pullalarevu <apullalarevu-c@sbgtv.com>
2018-12-07 12:02:22 +0530

    naming changes

--- src/com/test/DemoPositive.java
+++ src/com/test/DemoPositive.java
@@ -3,33 +3,32 @@ package com.test;
 import main.generics.BaseTest;
 import main.generics.CommonMethods;
 import main.generics.Utilities;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class DemoPositive extends BaseTest {
     @Test
     public void test()throws NullPointerException{
-        String unBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "UnTb", "LocatorType");
-        String unVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "UnTb", "LocatorValue");
-        String pwBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "PwTb", "LocatorType");
-        String pwval = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "PwTb", "LocatorValue");
-        String chBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "ChBox", "LocatorType");
-        String chVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "ChBox", "LocatorValue");
-        String loginBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginBTN", "LocatorType");
-        String loginval = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginBTN", "LocatorValue");
-        String addTaskBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "AddTasksButton", "LocatorType");
-        String addTaskVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "AddTasksButton", "LocatorValue");
+        String loginUserNameBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "UsernameTextBox", "LocatorType");
+        String loginUserNameVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "UsernameTextBox", "LocatorValue");
+        String loginPasswordBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "PasswordTextBox", "LocatorType");
+        String loginPasswordVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "PasswordTextBox", "LocatorValue");
+        String loginCheckBoxBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginCheckBox", "LocatorType");
+        String loginCheckBoxVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginCheckBox", "LocatorValue");
+        String loginBTNBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginBTN", "LocatorType");
+        String loginBTNval = Utilities.getcellData_OBR_PC(OBJECT_REPO, "LoginPage", "LoginBTN", "LocatorValue");
+        String addTaskLinkBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "AddTasksLink", "LocatorType");
+        String addTaskLinkVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "AddTasksLink", "LocatorValue");
         String searchBoxBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "SearchBox", "LocatorType");
         String searchBoxVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "SearchBox", "LocatorValue");
-        String newTaskBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "NewTaskLink", "LocatorType");
-        String newTaskVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "NewTaskLink", "LocatorValue");
-        String menuEleBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "ToolsQA", "menuLink", "LocatorType");
-        String menuEleVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "ToolsQA", "menuLink", "LocatorValue");
+        String addNewTaskBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "NewTaskLink", "LocatorType");
+        String addNewTaskVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "HomePage", "NewTaskLink", "LocatorValue");
+        String menuElementBy = Utilities.getcellData_OBR_PC(OBJECT_REPO, "ToolsQA", "menuLink", "LocatorType");
+        String menuElementVal = Utilities.getcellData_OBR_PC(OBJECT_REPO, "ToolsQA", "menuLink", "LocatorValue");
         String loginPageTitle = Utilities.getcellData_Test(TEST_DATA, "ActiTIME", "LoginPage");
         String homePageTitle = Utilities.getcellData_Test(TEST_DATA, "ActiTIME", "HomePage");
         String userName = Utilities.getcellData_Test(TEST_DATA, "ActiTIME", "Username");
         String pasword = Utilities.getcellData_Test(TEST_DATA, "ActiTIME", "Password");
-        String qaURL = Utilities.getcellData_Test(TEST_DATA, "ToolsQA", "URL");
+        String qaURL = Utilities.getcellData_Test(TEST_DATA, "ToolsQA", "ToolsQAURL");
         String pageTitle = Utilities.getcellData_Test(TEST_DATA, "ToolsQA", "PageTitle");
 
 
@@ -38,18 +37,18 @@ public class DemoPositive extends BaseTest {
         method.verifyTitle(loginPageTitle);
         String url = method.getCurrentUrl();
         method.verifyURL(url);
-        method.visibilityOfElement(unBy,unVal,"UserName Text Box");
-        method.sendKeys(unBy,unVal,userName,"UserName");
-        method.sendKeys(pwBy,pwval,pasword,"Password");
-        method.click(chBy,chVal,"ChBox");
-        method.getText(loginBy,loginval,"loginBTN");
-        method.click(loginBy,loginval,"loginBTN");
+        method.visibilityOfElement(loginUserNameBy,loginUserNameVal,"UserName Text Box");
+        method.sendKeys(loginUserNameBy,loginUserNameVal,userName,"UserName");
+        method.sendKeys(loginPasswordBy,loginPasswordVal,pasword,"Password");
+        method.click(loginCheckBoxBy,loginCheckBoxVal,"ChBox");
+        method.getText(loginBTNBy,loginBTNval,"loginBTN");
+        method.click(loginBTNBy,loginBTNval,"loginBTN");
         method.verifyTitleContain(homePageTitle);
         String url1 = method.getCurrentUrl();
         method.verifyURL(url1);
-        method.highLightElement(addTaskBy,addTaskVal,"addTasksButton");
+        method.highLightElement(addTaskLinkBy,addTaskLinkVal,"addTasksButton");
         method.isEnabled(searchBoxBy,searchBoxVal,"SearchBox");
-        method.clickByActions(newTaskBy,newTaskVal,"NewTask");
+        method.clickByActions(addNewTaskBy,addNewTaskVal,"NewTask");
         method.sleep(5);
 
         driver.navigate().to(qaURL);
@@ -57,7 +56,7 @@ public class DemoPositive extends BaseTest {
         method.verifyTitleContain(pageTitle);
         String QAurl = method.getCurrentUrl();
         method.verifyURL(QAurl);
-        method.moveToElement(menuEleBy, menuEleVal, "Selenium in Java");
+        method.moveToElement(menuElementBy, menuElementVal, "Selenium in Java");
 
         method.sleep(5);
 
